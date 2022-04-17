50f65f6a3a1fcf4e6f566621e7ea3dd3f55e16af
Tahmidd <tahmid.ratul6@gmail.com>
2022-04-17 15:22:50 +0600

    checkout added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Home from './Pages/Home/Home/Home';
 import Footer from './Pages/Shared/Footer/Footer';
 import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
 import Login from './Pages/Login/Login/Login';
+import RequireAuth from './Pages/Login/RequireAuth/RequireAuth';
+import Checkout from './Pages/Checkout/Checkout';
 
 function App() {
   return (
@@ -16,6 +18,11 @@ function App() {
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/service/:serviceId' element={<ServiceDetail></ServiceDetail>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path="/checkout" element={
+          <RequireAuth>
+            <Checkout></Checkout>
+          </RequireAuth>
+        }></Route>
       </Routes>
       <Footer></Footer>
     </div>
---
+++ src/Pages/Checkout/Checkout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Checkout = () => {
+    return (
+        <div>
+            <h2>Thank you for buying</h2>
+        </div>
+    );
+};
+
+export default Checkout;
