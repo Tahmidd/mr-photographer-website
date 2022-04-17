5874d22e22c3605b409066e5b54d3e9b373b89f5
Tahmidd <tahmid.ratul6@gmail.com>
2022-04-17 13:30:53 +0600

    footer added

--- src/Pages/Home/Overview/Overview.js
+++ src/Pages/Home/Overview/Overview.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import view from '../../../images/tiger-gbc0ec6835_1920.jpg'
+import './Overview.css'
 
 const Overview = () => {
     return (
         <div>
-            <div class="container my-3 py-3 ">
-                <div class="row gy-5 d-flex align-items-center">
-                    <div class="col-12 col-md-12 col-lg-6">
+            <div className="container my-3 py-3 ">
+                <div className="row gy-5 d-flex align-items-center">
+                    <div className="col-12 col-md-12 col-lg-6">
                         <h1> <span>Explore the WildLife.</span> <br /></h1>
                         <p>Come look at the world through my lens as
                             <br /> you learn about different animals.
@@ -15,7 +16,7 @@ const Overview = () => {
                         <button
                             type="button" class="btn btn-success"><i class="fas fa-shopping-cart"></i> See today</button>
                     </div>
-                    <div class="col-12 col-md-12 col-lg-6">
+                    <div className="col-12 col-md-12 col-lg-6">
                         <img className='img-fluid' src={view} alt="" />
                     </div>
                 </div>
---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCopyright } from '@fortawesome/free-solid-svg-icons'
+
+const Footer = () => {
+    const today = new Date();
+    const year = today.getFullYear();
+    return (
+        <div>
+            <footer className='text-center mt-5 bg-secondary h-100' >
+                <h3 className='pt-3'>All Rights Reserved by Mr. Photographer</h3>
+                <p><small>copyright <FontAwesomeIcon className='text-white' icon={faCopyright} /> {year} </small></p>
+                <p className='text-white pb-3'>Feel Free to Contact anytime</p>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
